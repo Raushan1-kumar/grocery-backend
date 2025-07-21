fc1374a363a17e0eda35dff5610a892d5518566d
raushan <raushansahu147@gmail.com>
2025-07-21 15:37:25 +0530

    model changes

--- server.js
+++ server.js
@@ -19,5 +19,5 @@ app.use('/api/cart', require('./routes/cartRoute'));
 app.use('/api/users', require('./routes/userRoutes'));
 
 // Start server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
